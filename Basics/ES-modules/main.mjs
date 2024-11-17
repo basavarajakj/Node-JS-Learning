5c7f36c3de935ac1a5a59c1858fb601fa7ddeff0
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-11-17 22:18:18 +0530

    ES modules

---
+++ Basics/ES-modules/main.mjs
@@ -0,0 +1,18 @@
+// import add from "./math-esm.mjs";
+// console.log(add(5, 3));
+
+//Pattern3
+// import math from "./math-esm.mjs";
+
+// const { add, subtract } = math;
+
+// console.log(add(2,3));
+// console.log(subtract(2,3));
+
+// Pattern04
+// import * as math from './math-esm.mjs';
+import { addition } from "./math-esm.mjs";
+
+const { addition } = math;
+
+console.log(addition(2,3));
---
+++ Basics/ES-modules/math-esm.mjs
@@ -0,0 +1,29 @@
+// const add = (a, b) => {
+//   return a+b
+// };
+
+// export default add;
+
+// Patter 2
+// export default  (a, b) => {
+//   return a+b
+// };
+
+// Pattern 3
+// const add = (a, b) => {
+//   return a+b
+// };
+
+// const subtract = (a, b) => {
+//   return a-b
+// };
+
+// export default {
+//   add,
+//   subtract
+// }
+
+// Patter4
+export const addition = (a,b) => {
+  return a+b
+}
